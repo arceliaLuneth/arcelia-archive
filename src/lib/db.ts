6efd64b8687aff97081bbf06b35388c3987b96e6
arcelialuneth <fauzancheca10@gmail.com>
2026-07-14 14:08:59 +0700

    configure Aiven database connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,9 +6,15 @@ if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_NAME) {
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
